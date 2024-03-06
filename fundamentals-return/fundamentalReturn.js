7aec3a8c605df6ba5fdd8313a458487936f90e1b
umarhadimukti <umarhadimukti@gmail.com>
2024-03-06 08:29:36 +0700

    master: fundamentals-return challenge

---
+++ fundamentals-return/fundamentalReturn.js
@@ -0,0 +1,11 @@
+const add = (num1, num2) => num1 + num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+const mod = (num1, num2) => num1 % num2;
+
+const exponent = (num1, num2) => num1 ** num2;
+
+const subt = (num1, num2) => num1 -= num2;
