076a2bec2a94c829bb5e2c38787994dbccacdbdf
umarhadimukti <umarhadimukti@gmail.com>
2024-03-15 06:32:16 +0700

    minMax: nambahin one line code

--- the-highest-profit-wins/minMax.js
+++ the-highest-profit-wins/minMax.js
@@ -1,9 +1,15 @@
+// one line code
+const minMaxOneLine = (arr) => [Math.min(...arr), Math.max(...arr)];
+console.log(minMaxOneLine([1,5,3,7]))
+
+// cara 2
 const minMax1 = (arr) => {
   const myArray = arr.sort();
   return [myArray[0], myArray[myArray.length-1]]
 }
 console.log(minMax1([1,3,2,5,4]))
 
+// cara 3
 const minMax = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length-i-1; j++) {
