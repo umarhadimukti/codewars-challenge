83ce08c9688224b1f57b84c2307e93b206a585e8
umarhadimukti <umarhadimukti@gmail.com>
2024-03-14 21:50:02 +0700

    cockroach: gantiin Math.floor pake ~~ NOT bitwise

--- cockroach/cockroach.js
+++ cockroach/cockroach.js
@@ -1,2 +1,3 @@
-const cockroachSpeed = (s) => Math.floor((s * 100000) / 3600)
+const cockroachSpeed = (s) => ~~((s * 100000) / 3600)
+// const cockroachSpeed = (s) => Math.floor((s * 100000) / 3600)
 console.log(cockroachSpeed(1.08));
