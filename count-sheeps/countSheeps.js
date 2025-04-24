9f0d5c13ffe5a939df1b7b488eb39b9145b7b625
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-24 21:15:22 +0700

    solution#2 countSheeps

--- count-sheeps/countSheeps.js
+++ count-sheeps/countSheeps.js
@@ -9,6 +9,7 @@
 //   true,  true,  true,  true ,
 //   false, false, true,  true]
 
+// solution#1
 const countSheeps = (sheep) => {
     let result = 0;
     sheep.forEach(s => result += s === true);
@@ -22,3 +23,16 @@ console.log(countSheeps([
     false,false,false,false,
     true,false,true,false
 ]));
+
+
+// solution#2
+const countSheeps2 = (sheep) => {
+    return sheep.filter(Boolean).length;
+}
+console.log(countSheeps2([
+    true,true,false,false,
+    false,true,true,true,
+    true,true,true,true,
+    false,false,false,false,
+    true,false,true,false
+]))
