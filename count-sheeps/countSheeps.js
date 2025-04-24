a5b66e0b9d4494213e0f1fd8501f57d9cffdcba0
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-24 21:13:35 +0700

    add countSheeps instruction

--- count-sheeps/countSheeps.js
+++ count-sheeps/countSheeps.js
@@ -1,3 +1,14 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
 const countSheeps = (sheep) => {
     let result = 0;
     sheep.forEach(s => result += s === true);
