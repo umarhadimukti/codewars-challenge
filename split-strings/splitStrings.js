7a0961ddd23bfcc98e9faaa4a878693bbcd5c588
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-07 13:36:18 +0700

    solution split strings

--- split-strings/splitStrings.js
+++ split-strings/splitStrings.js
@@ -5,3 +5,21 @@
 // * 'abc' =>  ['ab', 'c_']
 // * 'abcdef' => ['ab', 'cd', 'ef']
 
+const splitInPairs = (str) => {
+    // check if the length of the string is odd
+    if (str.length % 2 === 1) {
+        // if it is, add an underscore to the end of the string
+        str += '_';
+    }
+
+    // create an empty array to hold the pairs
+    const pairs = [];
+
+    // loop through the string in steps of 2
+    for (let i = 0; i < str.length; i += 2) {
+        // substring the string from i to i+2 and push it to the pairs array
+        pairs.push(str.substring(i, i+2));
+    }
+
+    return pairs;
+}
